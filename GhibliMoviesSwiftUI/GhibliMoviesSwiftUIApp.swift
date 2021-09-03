a38a1e3ab5c1d69d0d6c7d3f947a525a1dcf1a15
Clément Cardonnel <clement.cardonnel@me.com>
2021-09-03 15:30:03 +0200

    Initial Commit

---
+++ GhibliMoviesSwiftUI/GhibliMoviesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliMoviesSwiftUIApp.swift
+//  GhibliMoviesSwiftUI
+//
+//  Created by Clément Cardonnel on 03/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct GhibliMoviesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
